d1356b18c13a1e6c331544e1e138ee7b2f6e2904
Rae Haley <alyssarae98ha@gmail.com>
2026-07-08 23:39:14 -0600

    created professional about page

--- src/router/index.js
+++ src/router/index.js
@@ -2,10 +2,12 @@ import { createRouter, createWebHistory } from 'vue-router'
 // import HomeView from '../views/HomeView.vue'
 import Home from '../views/Home.vue';
 import PersonalAbout from '../views/PersonalAbout.vue';
+import ProfessionalAbout from '../views/ProfessionalAbout.vue';
 
 const routes = [
   { path: '/', component: Home },
-  { path: '/personal-about', component: PersonalAbout }
+  { path: '/personal-about', component: PersonalAbout },
+  { path: '/professional-about', component: ProfessionalAbout }
 ];
 
 const router = createRouter({
